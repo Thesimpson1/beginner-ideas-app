b7c1d3051bdb1d33120a9c64fe84afd0236a0252
Kulish Artur <artur@>
2023-08-02 15:02:25 +0200

    feat: added code structure

---
+++ app/screens/Authentication/StartScreen/StartScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+export function StartScreen() {
+  return (
+    <View>
+      <Text>Hello My Start Screen</Text>
+    </View>
+  );
+}
