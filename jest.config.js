f35d3e8fb5c1481e3f9690075ab7b7d53de5d9fd
Kulish Artur <artur@>
2023-08-03 19:48:19 +0200

    chore: configured jest tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  transformIgnorePatterns: [
+    '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$',
+    '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$',
+    '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$',
+  ],
 };
