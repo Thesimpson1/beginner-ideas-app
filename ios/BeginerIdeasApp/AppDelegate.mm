a86a94b66e7128951ded0419fd64d884d41bce04
Kulish Artur <artur@>
2023-09-18 18:18:12 +0200

    fix: data isn't cleared when app is uninstalled

--- ios/BeginerIdeasApp/AppDelegate.mm
+++ ios/BeginerIdeasApp/AppDelegate.mm
@@ -9,6 +9,7 @@
   self.moduleName = @"BeginerIdeasApp";
   [FIRApp configure];
   // You can add your custom initial props in the dictionary below.
+  if ([[NSUserDefaults standardUserDefaults] boolForKey:@"HAS_RUN_BEFORE"] == NO) {  [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"HAS_RUN_BEFORE"]; NSArray *secItemClasses = [NSArray arrayWithObjects: (__bridge id)kSecClassGenericPassword, (__bridge id)kSecClassInternetPassword, (__bridge id)kSecClassCertificate, (__bridge id)kSecClassKey, (__bridge id)kSecClassIdentity, nil]; for (id secItemClass in secItemClasses) { NSDictionary *spec = @{(__bridge id)kSecClass: secItemClass}; SecItemDelete((__bridge CFDictionaryRef)spec); } }
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
