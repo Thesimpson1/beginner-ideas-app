64021c55a07b5bf543379d5c6262b8b57cf24cde
Kulish Artur <artur@>
2023-09-27 16:39:57 +0200

    tests: test Home utils

--- app/screens/Home/utils/utils.test.tsx
+++ app/screens/Home/utils/utils.test.tsx
@@ -1,71 +1,35 @@
-import { validate } from 'app/screens/Authentication/Login/utils/utils';
+import React from 'react';
+import { HomeStackScreenName } from 'app/types';
 
-jest.mock('app/screens/Authentication/Login/utils/utils', () => {
-  const originalModule = jest.requireActual(
-    'app/screens/Authentication/Login/utils/utils'
-  );
+import { CalculatorIcon, CalendarIcon, NoteIcon } from 'app/assets/icon';
+import { validate } from 'app/screens/Authentication/Login/utils/utils';
+import { setIcon } from 'app/screens/Home/utils/utils';
+
+jest.mock('app/screens/Home/utils/utils', () => {
+  const originalModule = jest.requireActual('app/screens/Home/utils/utils');
 
   return {
     __esModule: true,
     ...originalModule,
   };
 });
-const validateMockProps = {
-  password: '',
-  email: '',
-};
-describe('Login utils', () => {
-  const mockPassword = 'mockPassword';
-  const mockPasswordMoreThan20 = 'mockPassworddddddddddd';
-  const mockEmail = 'mock@gmail.com';
-  const wrongEmailType = 'mockgmail.com';
-
-  it('case when password and email empty', () => {
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBe('Required');
-    expect(functionResult0?.password).toBe('Required');
-  });
-  it('case when password exist but email empty', () => {
-    validateMockProps.password = mockPassword;
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBe('Required');
-    expect(functionResult0?.password).toBe('');
-    validateMockProps.password = '';
-  });
-  it('case when password empty but email exist', () => {
-    validateMockProps.email = mockEmail;
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBe('');
-    expect(functionResult0?.password).toBe('Required');
-    validateMockProps.email = '';
-  });
-  it('case when password exist and more than 20 characters but email empty', () => {
-    validateMockProps.password = mockPasswordMoreThan20;
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBe('Required');
-    expect(functionResult0?.password).toBe('Must be 20 characters or less');
-    validateMockProps.password = '';
-  });
-  it('case when password empty but email exist and invalid', () => {
-    validateMockProps.email = wrongEmailType;
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBe('Invalid email address');
-    expect(functionResult0?.password).toBe('Required');
-    validateMockProps.email = '';
-  });
-  it('case when password and email exist and correct', () => {
-    validateMockProps.email = mockEmail;
-    validateMockProps.password = mockPassword;
-    const functionResult0 = validate(validateMockProps);
-
-    expect(functionResult0?.email).toBeUndefined();
-    expect(functionResult0?.password).toBeUndefined();
-    validateMockProps.email = '';
-    validateMockProps.password = '';
+describe('Home utils', () => {
+  it('set icon', () => {
+    const functionResult0 = setIcon();
+    expect(functionResult0).toStrictEqual(
+      <NoteIcon testID={'DefaultNoteIconTestID'} />
+    );
+    const functionResult1 = setIcon(HomeStackScreenName.CALCULATOR);
+    expect(functionResult1).toStrictEqual(
+      <CalculatorIcon testID={'CalculatorIconTestID'} />
+    );
+    const functionResult2 = setIcon(HomeStackScreenName.CALENDAR);
+    expect(functionResult2).toStrictEqual(
+      <CalendarIcon testID={'CalendarIconTestID'} />
+    );
+    const functionResult3 = setIcon(HomeStackScreenName.NOTES);
+    expect(functionResult3).toStrictEqual(
+      <NoteIcon testID={'NoteIconTestID'} />
+    );
   });
 });
--- app/screens/Home/utils/utils.tsx
+++ app/screens/Home/utils/utils.tsx
@@ -3,17 +3,17 @@ import { HomeStackScreenName } from 'app/types';
 
 import { CalculatorIcon, CalendarIcon, NoteIcon } from 'app/assets/icon';
 
-export const setIcon = (title: HomeStackScreenName) => {
-  let icon = <NoteIcon />;
+export const setIcon = (title?: HomeStackScreenName) => {
+  let icon = <NoteIcon testID={'DefaultNoteIconTestID'} />;
   switch (title) {
     case HomeStackScreenName.CALCULATOR:
-      icon = <CalculatorIcon />;
+      icon = <CalculatorIcon testID={'CalculatorIconTestID'} />;
       break;
     case HomeStackScreenName.CALENDAR:
-      icon = <CalendarIcon />;
+      icon = <CalendarIcon testID={'CalendarIconTestID'} />;
       break;
     case HomeStackScreenName.NOTES:
-      icon = <NoteIcon />;
+      icon = <NoteIcon testID={'NoteIconTestID'} />;
       break;
   }
   return icon;
