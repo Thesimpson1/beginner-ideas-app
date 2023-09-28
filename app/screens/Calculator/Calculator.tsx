0b9232d0dc78bb59485991712cfff9968d65ef61
Kulish Artur <artur@>
2023-09-28 20:25:44 +0200

    feat: logic to calculator (part 1)

--- app/screens/Calculator/Calculator.tsx
+++ app/screens/Calculator/Calculator.tsx
@@ -22,41 +22,22 @@ const calculatorData = [
   ['1', '2', '3', '+'],
   ['0', ',', '='],
 ];
-interface SetOpacityI {
-  text: string;
-  setIsClicked: Dispatch<SetStateAction<boolean>>;
-}
-const setOpacity = ({ text, setIsClicked }: SetOpacityI) => {
-  switch (true) {
-    case text === '/': {
-      return setIsClicked((prevState) => !prevState);
-    }
-    case text === 'X': {
-      return setIsClicked((prevState) => !prevState);
-    }
-    case text === '-': {
-      return setIsClicked((prevState) => !prevState);
-    }
-    case text === '+': {
-      return setIsClicked((prevState) => !prevState);
-    }
-  }
-};
+
 export function CalculatorScreen() {
-  const [isClicked, setIsClicked] = useState(false);
+  const [currentValue, setCurrentValue] = useState('0');
 
   const renderItem = ({ item, index }: RenderPropsI) => {
     const backgroundColor = getBackgroundColor({ item, index });
     const onPress = () => {
-      setOpacity({ setIsClicked, text: item });
+      // setOpacity({ setIsClicked, text: item });
     };
     return (
       <CalculatorRoundButton
         text={item}
-        isClicked={isClicked}
+        currentValue={currentValue}
         backgroundColor={backgroundColor}
         isLong={item === '0'}
-        onPress={onPress}
+        onPress={setCurrentValue}
         key={item.toString()}
       />
     );
@@ -76,7 +57,9 @@ export function CalculatorScreen() {
     <StyledCalculatorScreenContainer>
       <StyledCalculatorContentContainer>
         <StyledVisibleNumberContainer>
-          <StyledCalculatorRoundButtonText>101</StyledCalculatorRoundButtonText>
+          <StyledCalculatorRoundButtonText>
+            {currentValue}
+          </StyledCalculatorRoundButtonText>
         </StyledVisibleNumberContainer>
         <FlatList
           data={calculatorData}
--- app/screens/Calculator/components/CalculatorRoundButton/CalculatorRoundButton.tsx
+++ app/screens/Calculator/components/CalculatorRoundButton/CalculatorRoundButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import {
   StyledCalculatorRoundButtonContainer,
@@ -10,26 +10,54 @@ interface CalculatorRoundButtonI {
   backgroundColor?: string;
   color?: string;
   isLong?: boolean;
-  onPress: () => void;
-  isClicked?: boolean;
+  onPress: Dispatch<SetStateAction<string>>;
+  currentValue?: string;
   testID?: string;
 }
-
+interface CheckIsClickedI {
+  text: string;
+}
+const checkSigns = ({ text }: CheckIsClickedI) => {
+  let isRightSign = false;
+  switch (true) {
+    case text === '/': {
+      return (isRightSign = true);
+    }
+    case text === 'X': {
+      return (isRightSign = true);
+    }
+    case text === '-': {
+      return (isRightSign = true);
+    }
+    case text === '+': {
+      return (isRightSign = true);
+    }
+  }
+  return isRightSign;
+};
 export function CalculatorRoundButton({
   text,
   backgroundColor,
   color,
   isLong = false,
   onPress,
-  isClicked = false,
+  currentValue,
   testID = 'StyledCalculatorRoundButtonContainerTestID',
 }: CalculatorRoundButtonI) {
   return (
     <StyledCalculatorRoundButtonContainer
       backgroundColor={backgroundColor}
       isLong={isLong}
-      onPress={onPress}
-      isClicked={isClicked}
+      onPress={() =>
+        onPress((prevState) => {
+          if (prevState !== text) {
+            return text;
+          } else {
+            return '';
+          }
+        })
+      }
+      isClicked={currentValue === text && checkSigns({ text })}
       testID={testID}
     >
       <StyledCalculatorRoundButtonText
