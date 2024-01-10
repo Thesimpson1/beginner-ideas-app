bc64643c6d0c73e464a63111a0b9b2596952bb92
Kulish Artur <artur@>
2024-01-10 15:49:22 +0100

    test: added note tests to root-reducer

--- app/redux/root-reducer.test.ts
+++ app/redux/root-reducer.test.ts
@@ -1,10 +1,15 @@
-import { mockInitialState, mockInitialTimerState } from 'app/mocks';
+import {
+  mockInitialNotesState,
+  mockInitialState,
+  mockInitialTimerState,
+} from 'app/mocks';
 import { rootReducer } from 'app/redux/root-reducer';
 jest.mock('@react-native-async-storage/async-storage', () => 'AsyncStorage');
 describe('Root reducer', () => {
   const initState = {
     auth: mockInitialState,
     timer: mockInitialTimerState,
+    notes: mockInitialNotesState,
   };
 
   it('should return the initial state', () => {
